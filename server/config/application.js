8959e93b6eb6e58a2393ecea7d8ac639504f9f50
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-19 16:45:38 -0700

    Set environmental variables

--- server/config/application.js
+++ server/config/application.js
@@ -44,7 +44,11 @@ module.exports = (app) => {
    *
    *  For example: app.set('PG_HOST', process.env.PG_HOST || 'localhost')
    */
-
+  app.set('PORT', PORT);
+  app.set('PG_HOST', process.env.PG_HOST || 'localhost');
+  app.set('PG_USER', process.env.PG_USER || 'boomtown');
+  app.set('PG_PASSWORD', process.env.PG_PASSWORD || 'boomtown');
+  app.set('PG_DB', process.env.PG_DB || 'boomtown');
   app.use(cookieParser());
 
   if (process.env.NODE_ENV === 'production') {
