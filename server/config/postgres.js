82bdfd95bd5947be4bef51628332d36c3915d2db
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-19 16:47:55 -0700

    Create new pool with env variables

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,15 +1,13 @@
 const { Pool } = require('pg');
 
 module.exports = (app) => {
-  /**
-   * @TODO: Configuration Variables
-   *
-   *  Retrieve the necessary information to connect to Postgres
-   *  For example: app.get('PG_DB')
-   */
+  
   return new Pool({
-    /**
-     *  @TODO: Supply the correct configuration values to connect to postgres
-     */
+    host: app.get('PG_HOST'),
+    user: app.get('PG_USER'),
+    password: app.get('PG_PASSWORD'),
+    database: app.get('PG_DB'),
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
   });
 };
