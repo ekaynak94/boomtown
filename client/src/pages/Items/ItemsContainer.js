29cc198148668ca0471aec6e38158c36b36a4370
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-28 13:13:18 -0700

    Add query for testing

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import { } from '../../apollo/queries';
+//import FullScreenLoader from '../../components/FullScreenLoader';
+import { Query } from 'react-apollo';
+import { ALL_ITEMS_QUERY } from '../../apollo/queries';
 
 class ItemsContainer extends Component {
     render() {
         return (
-            <Items />
+            <Query query={ALL_ITEMS_QUERY} variables={{ filter: 1 }}>
+                {({ loading, error, data }) => {
+                    if (loading) return <p>Loading</p>;
+                    if (error) return <p>{`Error! ${error.message}`}</p>;
+                    if(data)console.log(data)
+                    return <Items classes={this.props.classes} items={data.items} />;
+                }}
+            </Query>
         );
     }
 }
