c70912987435ddcb542bad91d8c11ca90da7b0e2
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-29 15:34:37 -0700

    Create directories and files for new components

---
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SharePreview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <p>This is the share Preview.</p>
+      </div>
+    );
+  }
+}
+
+export default SharePreview;
