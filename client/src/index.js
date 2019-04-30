3fb4347149e86a469835d6166cbb9b3ea0e6aecf
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-30 15:13:18 -0700

    Add redux provider and cleanup comments

--- client/src/index.js
+++ client/src/index.js
@@ -2,50 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-// @TODO: Uncomment each module as needed in your client app
 import { ApolloProvider } from 'react-apollo'
  import { BrowserRouter } from 'react-router-dom'
-// import { Provider as ReduxProvider } from 'react-redux'
-// -------------------------------
+import { Provider as ReduxProvider } from 'react-redux'
+import store from './redux'
 
 import registerServiceWorker from './registerServiceWorker';
 import theme from './theme';
 import client from './apollo';
 import Layout from './routes/Layout'
-/**
- * @TODO: Initialize Apollo Client
- *
- * Uncomment the following line when Apollo Client is configured:
- *
- * import client from './apollo'
- *
- * Below in your <App />, wrap your pages in an <ApolloProvider /> component
- * and pass it `client` as the `client` prop value so they will
- * have access to data exposed by your GraphQL API.
- */
-
-/**
- * @TODO: Add Routing
- *
- * Uncomment the following line when your routes are configured
- *
- * import Layout from './routes/Layout'
- *
- * Below in your <App />, nest your <Routes /> inside of <BrowserRouter />
- * component to enable routing in your client app.
- */
-
-/**
- * @TODO: Initialize Redux Store
- *
- * Uncomment the following line when your Redux store is configured
- *
- * import store from './redux'
- *
- * Below in your <App />, wrap a <ReduxProvider /> component around all
- * of the app's children, and pass it the imported `store` as the `store`
- * prop's value.
- */
 
 /**
  * @TODO: Add the Viewer Context
@@ -62,14 +27,16 @@ import './index.css';
 
 const App = () => {
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline/>
-      <ApolloProvider client={client}> 
-        <BrowserRouter>
-          <Layout/>
-        </BrowserRouter>
-      </ApolloProvider>
-    </MuiThemeProvider>
+    <ReduxProvider store={store}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline/>
+        <ApolloProvider client={client}> 
+          <BrowserRouter>
+            <Layout/>
+          </BrowserRouter>
+        </ApolloProvider>
+      </MuiThemeProvider>
+    </ReduxProvider>
   );
 };
 
