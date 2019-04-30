ffcf496b53be030f64421e514def2bc3af148343
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-30 15:11:01 -0700

    Reducer for shareItem form

---
+++ client/src/redux/ShareItemPreview/reducer.js
@@ -0,0 +1,54 @@
+//actions
+const UPDATE_ITEM = "UPDATE_ITEM";
+const RESET_IMAGE = "RESET_IMAGE";
+const RESET_ITEM = "RESET_ITEM";
+
+//action creators
+export const updateItem = item => ({
+    type: UPDATE_ITEM,
+    payload: item
+});
+    
+export const resetImage = () => ({
+    type: RESET_IMAGE
+});
+
+export const resetItem = () => ({
+    type: RESET_ITEM
+});
+
+const initialState = {
+    title: "Name your item",
+    description: "Describe your item",
+    imageurl: "http://via.placeholder.com/350x250?text=Please+select+an+image",
+    tags: [],
+    itemowner: {
+    },
+    created: new Date()
+}
+
+//reducer
+export default (state = initialState, action) => {
+    switch (action.type) {
+        
+        case UPDATE_ITEM: 
+            return ({
+                ...state,
+                ...action.payload
+            });
+
+        case RESET_IMAGE: 
+            return ({
+                ...state,
+                imageurl: initialState.imageurl
+            });
+    
+        case RESET_ITEM: 
+            return ({
+                ...initialState
+            });
+        
+        default:
+            return state;
+    }
+}
