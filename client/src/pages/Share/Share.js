244676464a970212b50ab5a45249d685ec810b0f
Emre Kaynak <emrekaynak94@gmail.com>
2019-04-30 03:40:03 -0700

    Import and render shareitempreview and shareitemform components

--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -10,15 +10,14 @@ import React from 'react';
   Hint: It should look like any other Item card.
 
 */
-// import ShareItemForm from '../../components/ShareItemForm';
-// import ShareItemPreview from '../../components/ShareItemPreview';
+import ShareItemForm from '../../components/ShareItemForm';
+import ShareItemPreview from '../../components/ShareItemPreview';
 
 const Share = ({ classes }) => {
   return (
     <div>
-      <p>
-        This is the share page located at <code>/share</code>.
-      </p>
+      <ShareItemPreview />
+      <ShareItemForm/>
     </div>
   );
 };
