8c1c6c94aa25bf42b4f57a50abdeb6773e954d35
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-01 11:54:06 -0700

    Add mutations and related input types for signup and login

--- server/api/schema.js
+++ server/api/schema.js
@@ -2,53 +2,64 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
   scalar date
-  
+
   type Item {
-    id : ID!
-    title : String!
-    imageurl : String
-    description :String!
-    itemowner : User!
-    tags : [Tag]
-    borrower : User
+    id: ID!
+    title: String!
+    imageurl: String
+    description: String!
+    itemowner: User!
+    tags: [Tag]
+    borrower: User
     created: date!
   }
 
   type User {
-    id :ID!
-    email :String!
-    fullname :String!
-    bio :String
-    items :[Item]
-    borrowed :[Item]
+    id: ID!
+    email: String!
+    fullname: String!
+    bio: String
+    items: [Item]
+    borrowed: [Item]
   }
 
   type Tag {
-    id : ID!
-    title : String!
+    id: ID!
+    title: String!
   }
 
   type File {
-    id :ID!
-    filename :String!
-    mimetype :String!
-    encoding :String!
-    itemid :ID!
+    id: ID!
+    filename: String!
+    mimetype: String!
+    encoding: String!
+    itemid: ID!
   }
 
   input AssignedTag {
-    id : ID!
-    title : String!
+    id: ID!
+    title: String!
   }
 
   input AssignedBorrower {
-    id : ID!
+    id: ID!
   }
 
   input NewItemInput {
-    title : String!
-    description : String
-    tags : [AssignedTag]!
+    title: String!
+    description: String
+    tags: [AssignedTag]!
+  }
+
+  input SignupInput {
+    fullname: String!
+    email: String!
+    password: String!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
   }
 
   type Query {
@@ -59,6 +70,9 @@ module.exports = gql`
   }
 
   type Mutation {
-    addItem(item:NewItemInput!): Item
+    addItem(item: NewItemInput!): Item
+    login(user: LoginInput!): User!
+    logout: Boolean!
+    signup(user: SignupInput!): User!
   }
 `;
