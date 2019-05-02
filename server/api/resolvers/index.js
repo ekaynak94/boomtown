35cfc5dac294af9ac88ac8162844aeee7f626684
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-02 15:16:51 -0700

    Add viewer query

--- server/api/resolvers/index.js
+++ server/api/resolvers/index.js
@@ -25,21 +25,9 @@ module.exports = app => {
   return {
     
     //Date: DateScalar,
-    Query: { viewer() {
-        /**
-         * @TODO: Authentication - Server
-         *
-         *  If you're here, you have successfully completed the sign-up and login resolvers
-         *  and have added the JWT from the HTTP cookie to your resolver's context.
-         *
-         *  The viewer is what we're calling the current user signed into your application.
-         *  When the user signed in with their username and password, an JWT was created with
-         *  the user's information cryptographically encoded inside.
-         *
-         *  To provide information about the user's session to the app, decode and return
-         *  the token's stored user here. If there is no token, the user has signed out,
-         *  in which case you'll return null
-         */
+    Query: {
+      viewer( parent, args, {token}, info) {
+        if (token) return token;
         return null;
       }, async user(parent, { id }, { pgResource }, info) {
         try {
