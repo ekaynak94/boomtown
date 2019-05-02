df9ccbadb4eb11ea96c3ab9068812cd4a6f5182b
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-02 15:19:12 -0700

    Check if the viewer is logged in and route accordingly

--- client/src/routes/Layout.js
+++ client/src/routes/Layout.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
-
+import { ViewerContext } from '../context/ViewerProvider';
 import Home from '../pages/Home';
 import Items from '../pages/Items';
 import Profile from '../pages/Profile';
@@ -8,14 +8,29 @@ import Share from '../pages/Share';
 
 export default () => (
   <Fragment>
-    {/* @TODO: Add your menu component here */}
-    <Switch>
-      <Route path="/items" component={Items}/>
-      <Route path="/welcome" component={Home}/>
-      <Route path="/share" component={Share}/>
-      <Route path="/profile/:id" component={Profile}/>
-      <Redirect from="/*" to="/items" /> 
-      <Route />
-    </Switch>
+    <ViewerContext.Consumer>
+      {({ viewer, loading }) => {
+        if (loading) return <p>Loading...</p>;
+        if(viewer){
+          return(
+            /* @TODO: Add your menu component here */
+            <Switch>
+              <Route path="/items" component={Items} />
+              <Route path="/share" component={Share} />
+              <Route path="/profile/:id" component={Profile} />
+              <Redirect from="/*" to="/items" />
+            </Switch>
+          );
+        } else {
+          return (
+            <Switch>
+              <Route path="/welcome" component={Home} />
+              <Redirect from="/*" to="/welcome" /> 
+            </Switch>
+          );
+        }
+      }
+      }
+    </ViewerContext.Consumer>
   </Fragment>
 );
