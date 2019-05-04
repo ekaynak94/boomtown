66c9bcd40d2e738bfe619907779d07280696839e
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-04 14:09:10 -0700

    Style itemcard

--- client/src/components/ItemCard/styles.js
+++ client/src/components/ItemCard/styles.js
@@ -1,3 +1,23 @@
-const styles = theme => ({});
+const styles = theme => ({
+    card: {
+        maxWidth: 400,
+      },
+      cardMedia: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    avatar: {
+          borderRadius:'50%'
+    },
+    button: {
+        border: '1px solid grey',
+        padding: '0.5em 1em',
+        margin: '1.5em 0',
+        '&:hover': {
+           backgroundColor:'#FFFFF'
+        }
+    }
+      
+});
 
 export default styles;
