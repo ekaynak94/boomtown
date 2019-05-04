84cae306c78b88938c2c8015193ea73093e2ce63
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-03 18:11:31 -0700

    Add date scalar

--- server/api/custom-types.js
+++ server/api/custom-types.js
@@ -1,21 +1,23 @@
 const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 
-/**
- *  @TODO: Custom Types
- *  GraphQL includes the following built-in Scalar Types: https://graphql.org/learn/schema/#scalar-types
- *
- *  The purpose of the Scalar type is to validate the information being sent and received
- *  from our GraphQL API.
- *
- *  Apollo allows us to define our own custom types. We'll create our own custom type to handle the value from the 'created' field
- *  on the Item: https://www.apollographql.com/docs/graphql-tools/scalars.html
- *
- *  Once you've defined your custom DATE type, don't forget to add it to your schema.
- */
-
-// @TOOD: Refactor this into a custom DATE scalar type using new GraphQLScalarType()
-const DateScalar = undefined;
-// -------------------------------
+const DateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  parseValue(value) {
+    return new Date(value); // value from the client
+  },
+  serialize(value) {
+    return value.getTime(); // value sent to the client
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(ast.value); // ast value is always in string format
+    }
+    return null;
+  }
+}
+);
 
 module.exports = {
   DateScalar
