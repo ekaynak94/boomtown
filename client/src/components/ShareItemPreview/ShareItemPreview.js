ef107843bbece286652e0625b116b6c0b964f77f
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-04 13:42:56 -0700

    Get user info for shareitempreview from viewer context

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ItemCard from '../ItemCard';
 import { connect } from 'react-redux';
 
-const ShareItemPreview = ({shareItemPreview}) => {
+const ShareItemPreview = ({ user, shareItemPreview }) => {
+  if (user) {
+    shareItemPreview.itemowner={...user}
+  }
   return <ItemCard item={shareItemPreview} />;
   }
 
--- client/src/pages/Share/Share.js
+++ client/src/pages/Share/Share.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import ShareItemForm from '../../components/ShareItemForm';
 import ShareItemPreview from '../../components/ShareItemPreview';
+import {ViewerContext} from '../../context/ViewerProvider'
 
-const Share = ({ classes,tags }) => {
-  return (
-    <div>
-      <ShareItemPreview />
-      <ShareItemForm tags={tags}/>
-    </div>
-  );
-};
+const Share = ({ classes, tags }) => (
+  <ViewerContext.Consumer>
+    {({ viewer, loading }) => {
+        return (
+          <div>
+            <ShareItemPreview user={viewer}/>
+            <ShareItemForm tags={tags} />
+          </div>
+        );
+      }
+    }
+</ViewerContext.Consumer>
+)
 
 export default Share;
