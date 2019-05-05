c12cb38e3b9705971c67b1fbc09f879da4c2e3d5
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-05 07:22:26 -0700

    Styling for share page

--- client/src/pages/Share/styles.js
+++ client/src/pages/Share/styles.js
@@ -1,3 +1,14 @@
-const styles = theme => ({});
+const styles = theme => ({
+    Grid: {
+        margin: '0 auto',
+        width: '100%',
+        maxWidth:theme.container.maxWidth,
+        display: 'grid',
+        gridTemplateAreas:'"left right"',
+        gridTemplateColumns:'1fr',
+        justifyContent: 'center',
+        alignItems:'center',
+    },
+});
 
 export default styles;
