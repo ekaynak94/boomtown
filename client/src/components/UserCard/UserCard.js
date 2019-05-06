36cdf77776d6d4e070673f43da7a137c961689bd
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-06 04:22:20 -0700

    Markup for usercard

--- client/src/components/UserCard/UserCard.js
+++ client/src/components/UserCard/UserCard.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import styles from './styles';
 import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Gravatar from 'react-gravatar'
 
-const UserCard = ({ classes,user}) => {
+
+const UserCard = ({ classes, user }) => {
+    console.log(user)
     return (
-        <div>This is a usercard</div>
+        <Card className={classes.card}>
+            <CardHeader
+                avatar={
+                    <Gravatar email={user.email ? user.email : "example@example.com"} className={classes.avatar} />
+                }
+                title={user.fullname}
+            />
+            <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="headline" component="h2">
+                    {`${user.items.length} Item shared ${user.borrowed.length} Item borrowed`}
+                </Typography>
+                <Typography gutterBottom variant="subheading" component="h3">
+                    {user.bio?user.bio:'No bio provided'}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 }
 
