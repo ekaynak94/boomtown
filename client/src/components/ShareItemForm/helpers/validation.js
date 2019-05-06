f2c2d17f180133850bb9977209c6de5c4de7ba6d
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-06 08:24:06 -0700

    Add validation logic

--- client/src/components/ShareItemForm/helpers/validation.js
+++ client/src/components/ShareItemForm/helpers/validation.js
@@ -1,11 +1,13 @@
 export default function validate(values) {
   const errors = {};
-
-  /**
-   * @TODO: Write the validation rules for the share form.
-   *
-   * An item title, description, and at least one tag is required for all items.
-   */
-
+  if (!values.title) {
+    errors.name = 'Required';
+  }
+  if (!values.description) {
+    errors.description = 'Required';
+  }
+  if (!values.tags) {
+    errors.tags = 'Required';
+  }
   return errors;
 }
