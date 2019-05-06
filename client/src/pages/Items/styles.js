ed1e03099d7b9a03417b058646e963567e6c4f0a
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-06 05:43:17 -0700

    Styling for background

--- client/src/pages/Items/styles.js
+++ client/src/pages/Items/styles.js
@@ -1,3 +1,10 @@
-const styles = theme => ({});
+const styles = theme => ({
+    background: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: theme.palette.secondary.main,
+        paddingTop:100
+    },
+});
 
 export default styles;
--- client/src/pages/Profile/styles.js
+++ client/src/pages/Profile/styles.js
@@ -1,4 +1,10 @@
 const styles = theme => ({
+    background: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: theme.palette.secondary.main,
+        paddingTop:100
+    },
     root: {
         maxWidth: theme.container.maxWidth,
         margin:'0 auto'
