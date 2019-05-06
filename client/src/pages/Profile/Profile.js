12708f398edc570932197ef7fff5c8c66eb8377f
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-05 20:39:27 -0700

    Add all user items grid to component

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
-const Profile = ({ classes }) => {
+import ItemsGrid from '../../components/ItemsGrid'
+const Profile = ({ classes, user }) => {
   return (
     <div>
-      <p>
-        This is the profile page located at <code>/profile/:userId</code>.
-      </p>
+      <ItemsGrid items={user.items} classes={classes}/>
     </div>
   );
 };
