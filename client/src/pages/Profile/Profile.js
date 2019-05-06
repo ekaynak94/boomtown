66a265146e1db0e3047f542a8e1309bfdcff6de8
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-05 20:52:30 -0700

    Add title and usercard component

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import ItemsGrid from '../../components/ItemsGrid'
+import ItemsGrid from '../../components/ItemsGrid';
+import UserCard from '../../components/UserCard';
+import Typography from '@material-ui/core/Typography';
+
 const Profile = ({ classes, user }) => {
   return (
     <div>
+      <UserCard user={user} />
+      <Typography color="primary" variant="headline" component="h1">
+        Shared Items
+      </Typography>
       <ItemsGrid items={user.items} classes={classes}/>
     </div>
   );
