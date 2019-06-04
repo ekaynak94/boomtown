9508eb9bf8525342606cddb5c2db5f874694e20c
Emre Kaynak <emrekaynak94@gmail.com>
2019-06-04 15:19:42 -0700

    Add mutation to borrow items on button click

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -12,54 +12,73 @@ import Gravatar from 'react-gravatar';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { BORROW_ITEM_MUTATION } from '../../apollo/queries';
+import { Mutation } from 'react-apollo';
 
 const ItemCard = ({ classes, item }) => {
   return (
-    <Card className={classes.card}>
-      <Link to={`/profile/${item.itemowner.id}`}>
-        <CardMedia
-          className={classes.cardMedia}
-          image={
-            item.imageurl
-              ? item.imageurl
-              : 'http://via.placeholder.com/350x250?text=Please+select+an+image'
-          }
-          title="Image for item"
-        />
-        <CardHeader
-          avatar={
-            <Gravatar
-              email={
-                item.itemowner.email
-                  ? item.itemowner.email
-                  : 'example@example.com'
+    <Mutation mutation={BORROW_ITEM_MUTATION}>
+      {borrow => (
+        <Card className={classes.card}>
+          <Link to={`/profile/${item.itemowner.id}`}>
+            <CardMedia
+              className={classes.cardMedia}
+              image={
+                item.imageurl
+                  ? item.imageurl
+                  : 'http://via.placeholder.com/350x250?text=Please+select+an+image'
               }
-              className={classes.avatar}
+              title="Image for item"
             />
-          }
-          title={item.itemowner.fullname && item.itemowner.fullname}
-          subheader={moment(item.created)
-            .startOf('day')
-            .fromNow()}
-        />
-      </Link>
-      <CardContent className={classes.cardContent}>
-        <Typography variant="headline" component="h2">
-          {item.title && item.title}
-        </Typography>
-        <Typography>
-          {item.tags && item.tags.map(tag => tag.title).join(', ')}
-        </Typography>
-        <Typography noWrap variant="subheading" component="h3">
-          {item.description && item.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button className={classes.button} size="small">
-          Borrow
-        </Button>
-      </CardActions>
-    </Card>
+            <CardHeader
+              avatar={
+                <Gravatar
+                  email={
+                    item.itemowner.email
+                      ? item.itemowner.email
+                      : 'example@example.com'
+                  }
+                  className={classes.avatar}
+                />
+              }
+              title={item.itemowner.fullname && item.itemowner.fullname}
+              subheader={moment(item.created)
+                .startOf('day')
+                .fromNow()}
+            />
+          </Link>
+          <CardContent className={classes.cardContent}>
+            <Typography variant="headline" component="h2">
+              {item.title && item.title}
+            </Typography>
+            <Typography>
+              {item.tags && item.tags.map(tag => tag.title).join(', ')}
+            </Typography>
+            <Typography noWrap variant="subheading" component="h3">
+              {item.description && item.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            {item.borrower ? (
+              <Button className={classes.button} size="small">
+                Borrowed
+              </Button>
+            ) : (
+              <Button
+                className={classes.button}
+                size="small"
+                onClick={() => {
+                  borrow({ variables: { id: item.id } });
+                  window.location.reload();
+                }}
+              >
+                Borrow
+              </Button>
+            )}
+          </CardActions>
+        </Card>
+      )}
+    </Mutation>
   );
 };
 
